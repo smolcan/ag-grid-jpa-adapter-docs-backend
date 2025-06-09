00fedad25dafd5b8812298e2000cf87fcde046dd
smolcan <samuel.molcan01@gmail.com>
2025-06-09 21:26:24 +0200

    advanced filter docs example

--- src/main/java/io/github/smolcan/ag_grid_jpa_adapter_docs_backend/controller/docs/AdvancedFilterController.java
+++ src/main/java/io/github/smolcan/ag_grid_jpa_adapter_docs_backend/controller/docs/AdvancedFilterController.java
@@ -1,6 +1,12 @@
 package io.github.smolcan.ag_grid_jpa_adapter_docs_backend.controller.docs;
 
+import io.github.smolcan.ag_grid_jpa_adapter_docs_backend.service.docs.AdvancedFilterService;
+import io.github.smolcan.aggrid.jpa.adapter.request.ServerSideGetRowsRequest;
+import io.github.smolcan.aggrid.jpa.adapter.response.LoadSuccessParams;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/docs/filtering/advanced-filter")
 @RequiredArgsConstructor
 public class AdvancedFilterController {
+
+    private final AdvancedFilterService advancedFilterService;
+
+    @PostMapping("getRows")
+    public ResponseEntity<LoadSuccessParams> getRows(@RequestBody ServerSideGetRowsRequest request) {
+        LoadSuccessParams result = this.advancedFilterService.getRows(request);
+        return ResponseEntity.ok(result);
+    }
     
 }
--- src/main/java/io/github/smolcan/ag_grid_jpa_adapter_docs_backend/service/docs/AdvancedFilterService.java
+++ src/main/java/io/github/smolcan/ag_grid_jpa_adapter_docs_backend/service/docs/AdvancedFilterService.java
@@ -2,10 +2,14 @@ package io.github.smolcan.ag_grid_jpa_adapter_docs_backend.service.docs;
 
 import io.github.smolcan.ag_grid_jpa_adapter_docs_backend.model.entity.Trade;
 import io.github.smolcan.aggrid.jpa.adapter.column.ColDef;
+import io.github.smolcan.aggrid.jpa.adapter.exceptions.OnPivotMaxColumnsExceededException;
 import io.github.smolcan.aggrid.jpa.adapter.query.QueryBuilder;
+import io.github.smolcan.aggrid.jpa.adapter.request.ServerSideGetRowsRequest;
+import io.github.smolcan.aggrid.jpa.adapter.response.LoadSuccessParams;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AdvancedFilterService {
@@ -16,25 +20,46 @@ public class AdvancedFilterService {
     public AdvancedFilterService(EntityManager entityManager) {
         this.queryBuilder = QueryBuilder.builder(Trade.class, entityManager)
                 .colDefs(
-                        // enabled sorting
                         ColDef.builder()
                                 .field("tradeId")
                                 .filter(false)
-                                .sortable(true)
                                 .build(),
-                        // disabled sorting
+                        // strings
                         ColDef.builder()
                                 .field("product")
-                                .filter(false)
-                                .sortable(false)
                                 .build(),
-                        // disabled sorting - throws
                         ColDef.builder()
                                 .field("portfolio")
-                                .filter(false)
-                                .sortable(false)
+                                .build(),
+                        ColDef.builder()
+                                .field("book")
+                                .build(),
+                        // numbers
+                        ColDef.builder()
+                                .field("submitterId")
+                                .build(),
+                        ColDef.builder()
+                                .field("submitterDealId")
+                                .build(),
+                        // date
+                        ColDef.builder()
+                                .field("birthDate")
+                                .build(),
+                        // boolean
+                        ColDef.builder()
+                                .field("isSold")
                                 .build()
                 )
+                .enableAdvancedFilter(true)
                 .build();
+    }
+    
+    @Transactional(readOnly = true)
+    public LoadSuccessParams getRows(ServerSideGetRowsRequest request) {
+        try {
+            return this.queryBuilder.getRows(request);
+        } catch (OnPivotMaxColumnsExceededException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
