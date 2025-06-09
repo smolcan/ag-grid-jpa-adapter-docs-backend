a7ca820d7daca2ebdc196375a07748aa19a17b20
smolcan <samuel.molcan01@gmail.com>
2025-06-09 22:06:37 +0200

    aggregation docs example

---
+++ src/main/java/io/github/smolcan/ag_grid_jpa_adapter_docs_backend/controller/docs/AggregationController.java
@@ -0,0 +1,38 @@
+package io.github.smolcan.ag_grid_jpa_adapter_docs_backend.controller.docs;
+
+import io.github.smolcan.ag_grid_jpa_adapter_docs_backend.service.docs.AggregationService;
+import io.github.smolcan.aggrid.jpa.adapter.request.ServerSideGetRowsRequest;
+import io.github.smolcan.aggrid.jpa.adapter.response.LoadSuccessParams;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/docs/aggregation")
+@RequiredArgsConstructor
+public class AggregationController {
+
+    private final AggregationService aggregationService;
+
+    @PostMapping("getRows")
+    public ResponseEntity<LoadSuccessParams> getRows(@RequestBody ServerSideGetRowsRequest request) {
+        LoadSuccessParams result = this.aggregationService.getRows(request);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/suppress-agg-filtered-only/getRows")
+    public ResponseEntity<LoadSuccessParams> getRowsSuppressAggFilteredOnly(@RequestBody ServerSideGetRowsRequest request) {
+        LoadSuccessParams result = this.aggregationService.getRowsSuppressAggFilteredOnly(request);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/group-agg-filtering/getRows")
+    public ResponseEntity<LoadSuccessParams> getRowsGroupAggFiltering(@RequestBody ServerSideGetRowsRequest request) {
+        LoadSuccessParams result = this.aggregationService.getRowsGroupAggFiltering(request);
+        return ResponseEntity.ok(result);
+    }
+
+}
---
+++ src/main/java/io/github/smolcan/ag_grid_jpa_adapter_docs_backend/service/docs/AggregationService.java
@@ -0,0 +1,157 @@
+package io.github.smolcan.ag_grid_jpa_adapter_docs_backend.service.docs;
+
+import io.github.smolcan.ag_grid_jpa_adapter_docs_backend.model.entity.Trade;
+import io.github.smolcan.aggrid.jpa.adapter.column.ColDef;
+import io.github.smolcan.aggrid.jpa.adapter.exceptions.OnPivotMaxColumnsExceededException;
+import io.github.smolcan.aggrid.jpa.adapter.filter.provided.simple.AgDateColumnFilter;
+import io.github.smolcan.aggrid.jpa.adapter.filter.provided.simple.AgNumberColumnFilter;
+import io.github.smolcan.aggrid.jpa.adapter.query.QueryBuilder;
+import io.github.smolcan.aggrid.jpa.adapter.request.ServerSideGetRowsRequest;
+import io.github.smolcan.aggrid.jpa.adapter.response.LoadSuccessParams;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AggregationService {
+
+    private final QueryBuilder<Trade> queryBuilder;
+    private final QueryBuilder<Trade> queryBuilderSuppressAggFilteredOnly;
+    private final QueryBuilder<Trade> queryBuilderGroupAggFiltering;
+
+    @Autowired
+    public AggregationService(EntityManager entityManager) {
+        this.queryBuilder = QueryBuilder.builder(Trade.class, entityManager)
+                .colDefs(
+                        ColDef.builder()
+                                .field("product")
+                                .enableRowGroup(true)
+                                .build(),
+                        ColDef.builder()
+                                .field("portfolio")
+                                .enableRowGroup(true)
+                                .build(),
+                        ColDef.builder()
+                                .field("book")
+                                .enableRowGroup(true)
+                                .build(),
+                        // numbers
+                        ColDef.builder()
+                                .field("submitterId")
+                                .enableValue(true)
+                                .filter(new AgNumberColumnFilter())
+                                .build(),
+                        ColDef.builder()
+                                .field("submitterDealId")
+                                .enableValue(true)
+                                .filter(new AgNumberColumnFilter())
+                                .build(),
+                        // date
+                        ColDef.builder()
+                                .field("birthDate")
+                                .enableValue(true)
+                                .filter(new AgDateColumnFilter())
+                                .build()
+                )
+                .build();
+
+        this.queryBuilderSuppressAggFilteredOnly = QueryBuilder.builder(Trade.class, entityManager)
+                .colDefs(
+                        ColDef.builder()
+                                .field("product")
+                                .enableRowGroup(true)
+                                .build(),
+                        ColDef.builder()
+                                .field("portfolio")
+                                .enableRowGroup(true)
+                                .build(),
+                        ColDef.builder()
+                                .field("book")
+                                .enableRowGroup(true)
+                                .build(),
+                        // numbers
+                        ColDef.builder()
+                                .field("submitterId")
+                                .enableValue(true)
+                                .filter(new AgNumberColumnFilter())
+                                .build(),
+                        ColDef.builder()
+                                .field("submitterDealId")
+                                .enableValue(true)
+                                .filter(new AgNumberColumnFilter())
+                                .build(),
+                        // date
+                        ColDef.builder()
+                                .field("birthDate")
+                                .enableValue(true)
+                                .filter(new AgDateColumnFilter())
+                                .build()
+                )
+                .suppressAggFilteredOnly(true)
+                .build();
+
+        this.queryBuilderGroupAggFiltering = QueryBuilder.builder(Trade.class, entityManager)
+                .colDefs(
+                        ColDef.builder()
+                                .field("product")
+                                .enableRowGroup(true)
+                                .build(),
+                        ColDef.builder()
+                                .field("portfolio")
+                                .enableRowGroup(true)
+                                .build(),
+                        ColDef.builder()
+                                .field("book")
+                                .enableRowGroup(true)
+                                .build(),
+                        // numbers
+                        ColDef.builder()
+                                .field("submitterId")
+                                .enableValue(true)
+                                .filter(new AgNumberColumnFilter())
+                                .build(),
+                        ColDef.builder()
+                                .field("submitterDealId")
+                                .enableValue(true)
+                                .filter(new AgNumberColumnFilter())
+                                .build(),
+                        // date
+                        ColDef.builder()
+                                .field("birthDate")
+                                .enableValue(true)
+                                .filter(new AgDateColumnFilter())
+                                .build()
+                )
+                .groupAggFiltering(true)
+                .build();
+    }
+
+    @Transactional(readOnly = true)
+    public LoadSuccessParams getRows(ServerSideGetRowsRequest request) {
+        try {
+            return this.queryBuilder.getRows(request);
+        } catch (OnPivotMaxColumnsExceededException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public LoadSuccessParams getRowsSuppressAggFilteredOnly(ServerSideGetRowsRequest request) {
+        try {
+            return this.queryBuilderSuppressAggFilteredOnly.getRows(request);
+        } catch (OnPivotMaxColumnsExceededException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public LoadSuccessParams getRowsGroupAggFiltering(ServerSideGetRowsRequest request) {
+        try {
+            return this.queryBuilderGroupAggFiltering.getRows(request);
+        } catch (OnPivotMaxColumnsExceededException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+}
