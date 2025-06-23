9614431984740cd3df45b3059009d397512a5ef5
smolcan <samuel.molcan01@gmail.com>
2025-06-23 13:17:05 +0200

    pivoting docs example

---
+++ src/main/java/io/github/smolcan/ag_grid_jpa_adapter_docs_backend/controller/docs/PivotingController.java
@@ -0,0 +1,26 @@
+package io.github.smolcan.ag_grid_jpa_adapter_docs_backend.controller.docs;
+
+import io.github.smolcan.ag_grid_jpa_adapter_docs_backend.service.docs.PivotingService;
+import io.github.smolcan.aggrid.jpa.adapter.request.ServerSideGetRowsRequest;
+import io.github.smolcan.aggrid.jpa.adapter.response.LoadSuccessParams;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/docs/pivoting")
+@RequiredArgsConstructor
+public class PivotingController {
+    
+    private final PivotingService pivotingService;
+
+    @PostMapping("getRows")
+    public ResponseEntity<LoadSuccessParams> getRows(@RequestBody ServerSideGetRowsRequest request) {
+        LoadSuccessParams result = this.pivotingService.getRows(request);
+        return ResponseEntity.ok(result);
+    }
+    
+}
---
+++ src/main/java/io/github/smolcan/ag_grid_jpa_adapter_docs_backend/service/docs/PivotingService.java
@@ -0,0 +1,59 @@
+package io.github.smolcan.ag_grid_jpa_adapter_docs_backend.service.docs;
+
+import io.github.smolcan.ag_grid_jpa_adapter_docs_backend.model.entity.Trade;
+import io.github.smolcan.aggrid.jpa.adapter.column.ColDef;
+import io.github.smolcan.aggrid.jpa.adapter.exceptions.OnPivotMaxColumnsExceededException;
+import io.github.smolcan.aggrid.jpa.adapter.query.QueryBuilder;
+import io.github.smolcan.aggrid.jpa.adapter.request.ServerSideGetRowsRequest;
+import io.github.smolcan.aggrid.jpa.adapter.response.LoadSuccessParams;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PivotingService {
+
+    private final QueryBuilder<Trade> queryBuilder;
+    
+    @Autowired
+    public PivotingService(EntityManager entityManager) {
+        this.queryBuilder = QueryBuilder.builder(Trade.class, entityManager)
+                .colDefs(
+                        ColDef.builder()
+                                .field("product")
+                                .enableRowGroup(true)
+                                .build(),
+                        
+                        
+                        ColDef.builder()
+                                .field("portfolio")
+                                .enablePivot(true)
+                                .build(),
+                        
+                        ColDef.builder()
+                                .field("currentValue")
+                                .enableValue(true)
+                                .build(),
+                        ColDef.builder()
+                                .field("previousValue")
+                                .enableValue(true)
+                                .build()
+                )
+                .build();
+    }
+
+
+    @Transactional(readOnly = true)
+    public LoadSuccessParams getRows(ServerSideGetRowsRequest request) {
+        try {
+            return this.queryBuilder.getRows(request);
+        } catch (OnPivotMaxColumnsExceededException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    
+    
+    
+}
